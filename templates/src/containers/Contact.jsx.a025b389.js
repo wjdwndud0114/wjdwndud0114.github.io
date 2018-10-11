cc083619dff6969a355995271641c8777e3647cb
wjdwndud0114 <wjdwndud0114@yahoo.com>
2018-10-10 20:02:26 -0700

    Fix intro flicker

--- templates/src/containers/Contact.jsx.a025b389.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{56:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=u(0),l=function(e){return e&&e.__esModule?e:{default:e}}(n);t.default=function(){return l.default.createElement("div",null,l.default.createElement("h1",null,"These are my works: SEGFAULT"))}}});
-//# sourceMappingURL=Contact.jsx.a025b389.js.map
